597e0aff27b42bbe1bcf975048eb2c2efe401498
Omid Madmolil <omidmadmolil74@gmail.com>
2025-06-10 12:52:29 +0330

    Adding localStorae

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
-import React from 'react';
-import { TaskContexts } from './TaskContext';
-import './App.css'
-import Item from './Item';
-import Task from './Task';
+import React from "react";
+import { TaskContexts } from "./TaskContext";
+import "./App.css";
+import Item from "./Item";
+import Task from "./Task";
 function App() {
-  const [taskItems , setTaskItems] = React.useState([
+  const tasksFor = [
     {
-      id : 1 ,
-      title : 'Task Number 1',
-      done : false
-    } ,
-     {
-      id : 2 ,
-      title : 'Task Number 2',
-      done : true
-    } ,
-     {
-      id : 3 ,
-      title : 'Task Number 3',
-      done : false
-    } ,
-     {
-      id : 4 ,
-      title : 'Task Number 4',
-      done : false
-    }
-  ])
+      id: 1,
+      title: "Task Number 1",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Task Number 2",
+      done: true,
+    },
+    {
+      id: 3,
+      title: "Task Number 3",
+      done: false,
+    },
+    {
+      id: 4,
+      title: "Task Number 4",
+      done: false,
+    },
+  ];
+  const [taskItems, setTaskItems] = React.useState(() => {
+    const storedValue = localStorage.getItem("stateValues");
+    return storedValue ? JSON.parse(storedValue) : tasksFor 
+  });
+  React.useEffect(() => {
+    localStorage.setItem("stateValues", JSON.stringify(taskItems));
+  }, [taskItems]);
   return (
-    <div className='min-h-screen bg-gray-800 flex justify-center items-center flex-col gap-10'>
-    <TaskContexts.Provider value={{
-      taskItems , setTaskItems
-    }}>
-          <Item />
-          <Task />
-    </TaskContexts.Provider>
-     
+    <div className="min-h-screen bg-gray-800 flex justify-center items-center flex-col gap-10">
+      <TaskContexts.Provider
+        value={{
+          taskItems,
+          setTaskItems,
+        }}
+      >
+        <Item />
+        <Task />
+      </TaskContexts.Provider>
     </div>
   );
 }
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
-import { TaskContexts } from './TaskContext'
+import React from "react";
+import { TaskContexts } from "./TaskContext";
 import { FaTrash } from "react-icons/fa";
 import { BsXSquareFill } from "react-icons/bs";
 import { BsCheckSquareFill } from "react-icons/bs";
 
-
-
-
 const Task = () => {
-    const {taskItems , setTaskItems} = React.useContext(TaskContexts)
-    function handleDelete(id){
-       const newTasks =  taskItems.filter(t => t.id !== id )
-       setTaskItems(newTasks)
-    }
-    function handleDoUndo(id){
-      const index = taskItems.findIndex(t => t.id === id)
-      const tasklocal = [...taskItems]
-      tasklocal[index].done = !tasklocal[index].done
-      setTaskItems([...tasklocal])
-    }
-    console.log(
-      taskItems
-    );
-    if(taskItems.length === 0){
-      return (
-        <h1 className='text-5xl text-amber-50'>There is no Task</h1>
-      )
-    }
-  return (
-    <div className='text-white'>
-        <ul className='space-y-5'>
-            {taskItems.map(t=> (
-                <li key={t.id} id={t.id} className={`w-200 py-5 px-5 text-2xl flex justify-between ${t.done ? 'bg-amber-700' : 'bg-blue-900'}`}>
-                    <h1>{t.title}</h1>
-                    <div className='flex gap-5 items-center'>
-                      <FaTrash onClick={()=> handleDelete(t.id)} />
-                      {t.done ? <BsXSquareFill onClick={()=> handleDoUndo(t.id)} /> : <BsCheckSquareFill onClick={()=> handleDoUndo(t.id)} /> }
-                    </div>
-                </li>
-            ))}
-        </ul>
-    </div>
-  )
-}
+  const { taskItems, setTaskItems } = React.useContext(TaskContexts);
+  function handleDelete(id) {
+    const newTasks = taskItems.filter((t) => t.id !== id);
+    setTaskItems(newTasks);
+  }
+  function handleDoUndo(id) {
+    const index = taskItems.findIndex((t) => t.id === id);
+    const tasklocal = [...taskItems];
+    tasklocal[index].done = !tasklocal[index].done;
+    setTaskItems([...tasklocal]);
+  }
 
-export default Task
+  if (taskItems.length === 0) {
+    return <h1 className="text-5xl text-amber-50">There is no Task</h1>;
+  }
+  return (
+    <div className="text-white">
+      <ul className="space-y-5">
+        {taskItems.map((t) => (
+          <li
+            key={t.id}
+            id={t.id}
+            className={`w-200 py-5 px-5 text-2xl flex justify-between ${
+              t.done ? "bg-amber-700" : "bg-blue-900"
+            }`}
+          >
+            <h1>{t.title}</h1>
+            <div className="flex gap-5 items-center">
+              <FaTrash onClick={() => handleDelete(t.id)} />
+              {t.done ? (
+                <BsXSquareFill onClick={() => handleDoUndo(t.id)} />
+              ) : (
+                <BsCheckSquareFill onClick={() => handleDoUndo(t.id)} />
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Task;
